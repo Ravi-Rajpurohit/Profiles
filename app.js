fb13369ca47b648354de37bbf2cb25f63e0ba0ea
Ravi <ravi.rajpurohit385@gmail.com>
2017-08-03 17:54:45 +0530

    added service to add profile

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
-angular.module('profile',[
+angular.module('profile', [
 
 ])
-.controller('ProfileCtrl', function($scope){
-    $scope.profiles = [
-        {"fName":"Ravi","lName":"Rajpurohit","age":22,"city":"Pune"},
-        {"fName":"Sujay","lName":"Pujari","age":25,"city":"Mumbai"},
-        {"fName":"Sanket","lName":"Rasane","age":27,"city":"Jaipur"},
-        {"fName":"Parth","lName":"Gharpure","age":19,"city":"Delhi"},
+    .controller('ProfileCtrl', function ($scope, ProfileService) {
+        $scope.profiles = ProfileService.getProfile();
+        $scope.addProfile = addProfile;
+
+        function addProfile(newProfile) {
+            ProfileService.addProfile(newProfile);
+
+        }
+    })
+
+
+angular.module('profile').service('ProfileService', function () {
+    var profiles = [
+        { "fName": "Ravi", "lName": "Rajpurohit", "age": 22, "city": "Pune" },
+        { "fName": "Sujay", "lName": "Pujari", "age": 25, "city": "Mumbai" },
+        { "fName": "Sanket", "lName": "Rasane", "age": 27, "city": "Jaipur" },
+        { "fName": "Parth", "lName": "Gharpure", "age": 19, "city": "Delhi" },
     ];
 
-
+    this.getProfile = function () {
+        return profiles;
+    }
 
 
     function addProfile(newProfile) {
         console.log("hiiiiiiiiiiiiiiiiiiiii");
-        $scope.profiles.push(newProfile);
+        profiles.push(newProfile);
     }
 
-    // $scope.add = function(){
-    //     console.log="hiii";
-    // }
-
-    $scope.addProfile = addProfile;
+    this.addProfile = addProfile;
 })
